3dfca7e4c733b9ee6f01c30aaa13c29feb11d7a7
adesina <sina.dev.engr@gmail.com>
2025-05-20 15:04:05 +0000

    some minor changes

--- app/Listeners/TradeRequestNotification.php
+++ app/Listeners/TradeRequestNotification.php
@@ -30,7 +30,7 @@ class TradeRequestNotification
         $recipient = User::where('uuid', $event->owner)->first();
 
 
-        Notification::route('sms', trim($user->mobile, '+'))->notify(new Traded(trim($user->mobile, '+'), 'Hi Ratefy User, ' . $recipient->firstname . ' has just placed  trade of  ' . $event->amount . 'units'));
+        Notification::route('sms', trim($user->mobile, '+'))->notify(new Traded(trim($user->mobile, '+'), 'Hi Ratefy User, ' . $recipient->firstname . ' has just placed  trade of  ' . $event->amount . ' units. Powered by Ratefy.'));
 
         $user->notify(new TradeRequestMail($event->amount, $event->amountInNaira, $user, $recipient, $event->item_id, $event->wallet_name, $event->item));
     }
